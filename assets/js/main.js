2214c08124696fefd4f8479331d71586f003f89c
Syed Ali <syedyusufali6626@gmail.com>
2024-09-12 00:28:31 +0530

    Major changes in internal  link redirection and added progress bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -290,5 +290,14 @@ document.addEventListener('DOMContentLoaded', function () {
 			
 })
 
-
-(jQuery);
+ (jQuery);
+ var element = document.documentElement,
+body = document.body,
+scrollTop = 'scrollTop',
+ scrollHeight = 'scrollHeight',
+ progress = document.querySelector('.progress-bar'),
+ scroll;
+ document.addEventListener('scroll', function() {
+	scroll = (element[scrollTop]||body[scrollTop]) / ((element[scrollHeight]||body[scrollHeight]) - element.clientHeight) * 100;
+	progress.style.setProperty('--scroll', scroll + '%');
+});
