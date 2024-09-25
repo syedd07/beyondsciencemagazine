18d3ef192fe5ffda1b2a22e103a7b593ffa8efe4
Syed Ali <syedyusufali6626@gmail.com>
2024-09-25 19:27:17 +0530

    PWA caching issue fixed

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 const CACHE_NAME = 'my-site-cache-v1';
 const urlsToCache = [
   '/',
-  '/assets/css/main.css',
-  '/assets/js/search.js',
-  '/assets/js/util.js',
-  '/assets/js/jquery.min.js',
-  '/assets/js/browser.min.js',
-  '/assets/js/breakpoints.min.js',
+  'assets/css/main.css',
+  'assets/js/search.js',
+  'assets/js/util.js',
+  'assets/js/jquery.min.js',
+  'assets/js/browser.min.js',
+  'assets/js/breakpoints.min.js',
   'assets/js/main.js',
-  '/index.html',
-  '/about.html',
-  '/contact.html',
-  '/e-books.html',
-  '/books.html',
-  '/articles.html',
+  'index.html',
+  'about.html',
+  'contact.html',
+  'e-books.html',
+  'books.html',
+  'articles.html',
   
   '/articles/images/favicon_io/icon-192x192.png'
 ];
