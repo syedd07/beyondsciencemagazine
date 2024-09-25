a4aeff7d079ce9ec35032679ef4b05a6660b67e9
Syed Ali <syedyusufali6626@gmail.com>
2024-09-25 19:47:00 +0530

    Fixed PWA try V3

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 const CACHE_NAME = 'my-site-cache-v1';
 const urlsToCache = [
   '/',
-  'assets/css/main.css',
-  'assets/js/search.js',
-  'assets/js/util.js',
-  'assets/js/jquery.min.js',
-  'assets/js/browser.min.js',
-  'assets/js/breakpoints.min.js',
-  'assets/js/main.js',
+  '/assets/css/main.css',
+  '/assets/js/search.js',
+  '/assets/js/util.js',
+  '/assets/js/jquery.min.js',
+  '/assets/js/browser.min.js',
+  '/assets/js/breakpoints.min.js',
+  '/assets/js/main.js',
   'index.html',
   'about.html',
-  'contact.html',
+  '/articles/contact.html',
   'e-books.html',
   'books.html',
-  'articles.html',
+  '/articles/article.html',
   
   'articles/images/favicon_io/icon-192x192.png'
 ];
